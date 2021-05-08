442ed2ebb09059563ad47bf63379f533cd11fef5
Edward Bent <bentorama@gmail.com>
2021-05-08 17:28:59 +0100

    Fixed pokemon inclusion validation

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,8 +1,7 @@
 class Pokemon < ApplicationRecord
-  # CATEGORIES = %w[Normal Fire Water Grass Electric Ice Fighting Poison Ground Flying Psychic Bug Rock Ghost Dragon]
+  CATEGORIES = %w[Normal Fire Water Grass Electric Ice Fighting Poison Ground Flying Psychic Bug Rock Ghost Dragon]
   belongs_to :user
   has_many :bookings, dependent: :destroy
-
+  validates :category, inclusion: { in: CATEGORIES, message: "%{value} is not a valid category" }
   validates :name, :category, :user_id, presence: true
-  # validates :category, inlcusion: { in: CATEGORIES, message: "%{value} is not a valid category" }
 end
