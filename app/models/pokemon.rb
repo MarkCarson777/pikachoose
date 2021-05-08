4c215667bad6cf573b400fedbd241d1823d04671
Edward Bent <bentorama@gmail.com>
2021-05-08 16:37:42 +0100

    Added validations and routes

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,8 @@
 class Pokemon < ApplicationRecord
+  # CATEGORIES = %w[Normal Fire Water Grass Electric Ice Fighting Poison Ground Flying Psychic Bug Rock Ghost Dragon]
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+
+  validates :name, :category, :user_id, presence: true
+  # validates :category, inlcusion: { in: CATEGORIES, message: "%{value} is not a valid category" }
 end
