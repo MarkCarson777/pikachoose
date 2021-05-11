b68dc8c616e3615f7c4c6b26f73c0927d9e6e549
harriet oughton <harrietoughton@gmail.com>
2021-05-11 20:12:01 +0100

    created one user and 50 Pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "creating user..."
+
+User.create!(
+  email: "bentorama@gmail.com",
+  password: "password"
+)
+
+puts "user created"
+
+puts "creating Pokemon..."
+
+50.times do
+  Pokemon.create!(
+    name: Faker::Games::Pokemon.name,
+    category: ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon"].sample,
+    price: Faker::Number.decimal_part(digits: 3),
+    user: User.first
+  )
+end
+puts "finished seeding"
