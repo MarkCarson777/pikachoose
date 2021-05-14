dfd1cd3e1ed765eb6a7e2afb563da07435492d28
Ed Sants <edward.sants@googlemail.com>
2021-05-14 11:46:33 +0100

    added dates to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   before_action :find_booking, only: [:show, :edit, :update, :destroy]
-  
+
   def show
   end
 
@@ -10,15 +10,18 @@ class BookingsController < ApplicationController
   end
 
   def create
-     # @booking = Booking.new(booking_params)
+    @booking = Booking.new(booking_params)
     @pokemon = Pokemon.find(params[:pokemon_id])
-    @booking = Booking.new(user: current_user, pokemon: @pokemon)
-    # @booking.pokemon = @pokemon
+    @booking.user = current_user
+    # @booking = Booking.new(user: current_user, pokemon: @pokemon)
+
+    @booking.pokemon = @pokemon
     if @booking.save!
     redirect_to pokemon_path(@pokemon)
     else
       render :new
     end
+
   end
 
   def edit
@@ -37,9 +40,9 @@ class BookingsController < ApplicationController
   def find_booking
     @booking = Booking.find(params[:id])
   end
-  
+
   def booking_params
-    params.require(:booking).permit(:user_id, :pokemon_id)
+    params.require(:booking).permit(:user_id, :pokemon_id, :start_date, :end_date)
   end
-  
+
 end
