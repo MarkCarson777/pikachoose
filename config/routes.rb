9f9f60e026fda9b5b65876a5906d71844bde9903
Edward Bent <bentorama@gmail.com>
2021-05-15 15:16:50 +0100

    Added My Pokemon and updated navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pokemons do
+    collection do
+      get :my_pokemon
+    end
     resources :bookings, only: [ :new, :create] 
   end
 
